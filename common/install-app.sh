ba492dfdab9b4775aee3b6c655a3aca049dd413e
Ribyn <robinmeow97@gmail.com>
2026-09-13 18:48:25 +0200

    hyprapps typo

--- common/install-app.sh
+++ common/install-app.sh
@@ -12,5 +12,5 @@ STACK_NAME=${1:?stack name is required as 1st arg}
 export HYRIBYN=${HYRIBYN:-"$HYRIBYN_ROOT/hyribyn"}
 export HYRIBYN_DISTRO=${HYRIBYN_DISTRO:-"fedora"}
 
-"$HYRIBYN/hyprapp/$STACK_NAME/install-deps-$HYRIBYN_DISTRO.sh"
-"$HYRIBYN/hyprapp/$STACK_NAME/build-and-install.sh"
+"$HYRIBYN/hyprapps/$STACK_NAME/install-deps-$HYRIBYN_DISTRO.sh"
+"$HYRIBYN/hyprapps/$STACK_NAME/build-and-install.sh"
