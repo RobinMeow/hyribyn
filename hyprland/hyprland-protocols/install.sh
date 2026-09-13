c4ef0854a0811cf27e34676562b4ea09b004418b
Ribyn <robinmeow97@gmail.com>
2026-09-13 14:26:21 +0200

    wip

--- hyprland/hyprland-protocols/install.sh
+++ hyprland/hyprland-protocols/install.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 export HYRIBYN=${HYRIBYN:-"$HYRIBYN_ROOT/hyribyn"}
+export HYRIBYN_DISTRO=${HYRIBYN_DISTRO:-"fedora"}
 
 "$HYRIBYN/hyprland/hyprland-protocols/install-deps-$HYRIBYN_DISTRO.sh"
-
-HYRIBYN_DISTRO=${HYRIBYN_DISTRO:-"fedora"} \
-	"$HYRIBYN/hyprland/hyprland-protocols/build-and-install.sh"
+"$HYRIBYN/hyprland/hyprland-protocols/build-and-install.sh"
